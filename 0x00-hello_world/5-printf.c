05dab015f0d54089cf5315763251b9663f35d033
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-10-05 14:26:20 -0700

    with proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+* main - Entry position
+*
+* Return: Always 0 (Succeed)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
