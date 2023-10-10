3c16b614f9ed0dfe955dd4575015b7b412d6c5ad
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-10-10 14:46:40 -0700

    find and print the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,7 +5,7 @@
  *
  * Definition - no hardcoding 
  *
- * Return:  (Successiful)
+ * Return: (Successiful)
  */
 
 int main(void)
@@ -41,7 +41,7 @@ int main(void)
 		bef1 = aft1 - bef1;
 		aft2 = aft2 + bef2;
 		bef2 = aft2 - bef2;
-}
+	}
 	printf("\n");
 	return (0);
 }
