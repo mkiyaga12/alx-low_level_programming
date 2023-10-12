0befc4687f0b4e848d76b66e3b752a4396c298fc
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-10-12 12:56:28 -0700

    is adigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - check if the numbers are 0 - 9
- * @c: parameter to be checked
- *
- * Return: 0 or 1
+ * _isdigit - checks if a char is digit
+ * @c: carrier variable
+ * Return: 1 if true, 0 if false
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-		return (1);
-	else
-		return (0);
+if (c >= '0' && c <= '9')
+return (1);
+
+else
+return (0);
 }
+
