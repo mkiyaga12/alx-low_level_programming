e08244ff6275286f440b5ef52530bcfdde194c9c
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-10-16 12:00:19 -0700

    prints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,10 +3,11 @@
 
 /**
  * print_array - prints n elements of an array
- * @a: array 
+ * @a: array
  * @n: is the number of elements to be printed
- * Return: a and n 
+ * Return: a and n
  */
+
 void print_array(int *a, int n)
 {
 	int i;
