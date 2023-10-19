0c765e877621b187a6e4b964453c1533a4521dad
mkiyaga12 <kiyagamuzamiru@gmail.com>
2023-10-19 09:05:13 -0700

    encode a string into 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * leet - switch vowels to numbers
+ * @s: analyzed string
+ *
+ * Return: String all vowels
+ */
+char *leet(char *s)
+{
+	char a[] = "aeotlAEOTL";
+	char n[] = "4307143071";
+	int i = 0;
+	int j;
+
+	while (*(s + i) != '\0')
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (*(s + i) == a[j])
+			{
+				*(s + i) = n[j];
+			}
+		}
+		i++;
+	}
+	return (s);
+}
